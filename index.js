6fc007e1d1f7ad8f2e38f3514f5843a7042ad401
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-01-04 07:42:58 -0500

    Added ability to add new display objects

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 //Theme switch function
 const themeToggleButton = document.querySelector("#theme-toggle");
 
+let currentTheme = "light";
 let style = window.getComputedStyle(document.body);
 const lightColor = style.getPropertyValue("--light-mode-color");
 const darkColor = style.getPropertyValue("--dark-mode-color");
-
-//Match the website theme to computer's chosen theme
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.style.setProperty("--primary-color", darkColor);
-    document.documentElement.style.setProperty("--secondary-color", lightColor);
-
-    let icons = document.querySelectorAll(".icon");
-
-    for (let i = 0; i < icons.length; i++) {
-        icons[i].classList.add("svg-light");
-    }
-}
 
 themeToggleButton.addEventListener("click", (e) => {
     let icons = document.querySelectorAll(".icon");
@@ -28,4 +17,35 @@ themeToggleButton.addEventListener("click", (e) => {
     let secondaryColor = style.getPropertyValue("--secondary-color");
     document.documentElement.style.setProperty("--primary-color", secondaryColor);
     document.documentElement.style.setProperty("--secondary-color", primaryColor);
+
+    currentTheme = (currentTheme) === "light" ? "dark" : "light";
+    console.log(currentTheme);
 });
+
+//DisplayObject class
+//Inputs are: div of .color-box
+class DisplayObject {
+    constructor(DOMObject) {
+        this.DOMObject = DOMObject;
+    }
+}
+
+//Add a new color box to the display
+let boxList = [];
+
+const contentBox = document.querySelector("#content");
+const addBox = document.querySelector("#add-display");
+const boxTemplate = document.querySelector("#display-template");
+
+addBox.addEventListener("click", (e) => {
+    let newBox = boxTemplate.content.cloneNode(true);
+
+    if (currentTheme === "dark") {
+        newBox.querySelectorAll(".icon").forEach((e) => {
+            e.classList.toggle("svg-light");
+        })
+    }
+
+    contentBox.appendChild(newBox);
+    boxList.push(new DisplayObject(newBox));
+})
