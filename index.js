30f8f52763a429a8640f2e292cbac77565e6c22e
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-01-04 07:09:52 -0500

    Light/dark theme toggle working

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 //Theme switch function
 const themeToggleButton = document.querySelector("#theme-toggle");
-let currentTheme = "light";
 
 let style = window.getComputedStyle(document.body);
-let lightColor = style.getPropertyValue("--light-mode-color");
-let darkColor = style.getPropertyValue("--dark-mode-color");
+const lightColor = style.getPropertyValue("--light-mode-color");
+const darkColor = style.getPropertyValue("--dark-mode-color");
 
+//Match the website theme to computer's chosen theme
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.style.setProperty("--current-theme-color", darkColor);
-    document.documentElement.style.setProperty("--current-secondary-color", lightColor);
+    document.documentElement.style.setProperty("--primary-color", darkColor);
+    document.documentElement.style.setProperty("--secondary-color", lightColor);
+
+    let icons = document.querySelectorAll(".icon");
+
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].classList.add("svg-light");
+    }
 }
+
+themeToggleButton.addEventListener("click", (e) => {
+    let icons = document.querySelectorAll(".icon");
+
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].classList.toggle("svg-light");
+    }
+
+    let primaryColor = style.getPropertyValue("--primary-color");
+    let secondaryColor = style.getPropertyValue("--secondary-color");
+    document.documentElement.style.setProperty("--primary-color", secondaryColor);
+    document.documentElement.style.setProperty("--secondary-color", primaryColor);
+});
