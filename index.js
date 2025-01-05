3ffd304bf1ca482b39b32211f21b23303ddc1e9a
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-01-05 10:25:14 -0500

    Added function for translating and verifying hex codes

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-//Theme switch function
+//Verify if a string is a valid hex code
+//String -> Boolean
+//True if valid, false otherwise
+function verifyHex(hexCode) {
+    let regex = /^#[0-9A-F]{6}$/i;
+
+    return regex.test(hexCode);
+}
+
+//Increase a hex color by input amounts
+//String(hex), int, int, int -> String(hex)
+function increaseHex(currentColor, rIncrease, gIncrease, bIncrease) {
+    let red = (parseInt(currentColor.slice(1, 3), 16) + rIncrease) % 255;
+    let green = (parseInt(currentColor.slice(3, 5), 16) + gIncrease) % 255;
+    let blue = (parseInt(currentColor.slice(5), 16) + bIncrease) % 255;
+
+    let finalHex = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+
+    return finalHex;
+}
+
+//Theme switch variables
 const themeToggleButton = document.querySelector("#theme-toggle");
 
 let currentTheme = "light";
@@ -6,6 +27,7 @@ let style = window.getComputedStyle(document.body);
 const lightColor = style.getPropertyValue("--light-mode-color");
 const darkColor = style.getPropertyValue("--dark-mode-color");
 
+//Theme switch function
 themeToggleButton.addEventListener("click", (e) => {
     let icons = document.querySelectorAll(".icon");
 
@@ -19,14 +41,58 @@ themeToggleButton.addEventListener("click", (e) => {
     document.documentElement.style.setProperty("--secondary-color", primaryColor);
 
     currentTheme = (currentTheme) === "light" ? "dark" : "light";
-    console.log(currentTheme);
 });
 
 //DisplayObject class
 //Inputs are: div of .color-box
 class DisplayObject {
+    #rIncrease = 0;
+    #gIncrease = 0;
+    #bIncrease = 0;
+    #intervalMS = 250;
+
     constructor(DOMObject) {
         this.DOMObject = DOMObject;
+    }
+
+    //String as hexadecimal color
+    setColor(hexColor) {
+        this.color = hexColor;
+        this.DOMObject.style.backgroundColor = hexColor;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+    setRIncrease(rIncrease) {
+        this.rIncrease = rIncrease;
+    }
+
+    getRIncrease() {
+        return this.rIncrease;
+    }
+    
+    setGIncrease(gIncrease) {
+        this.gIncrease = gIncrease;
+    }
+
+    getGIncrease() {
+        return this.gIncrease;
+    }
+    
+    setBIncrease(bIncrease) {
+        this.bIncrease = bIncrease;
+    }
+
+    getBIncrease() {
+        return this.bIncrease;
+    }
+
+    cycleColor() {
+        let newColor = increaseHex(this.color, this.rIncrease, this.gIncrease, this.bIncrease);
+
+        this.setColor(newColor);
     }
 }
 
@@ -49,3 +115,4 @@ addBox.addEventListener("click", (e) => {
     contentBox.appendChild(newBox);
     boxList.push(new DisplayObject(newBox));
 })
+
